09cd39ba5b024a5b757521b8c1da390b5a67d9dd
bouquet <bouquet@sio.ndlp>
2026-09-07 16:07:25 +0200

    [Change] Add currentDate on welcome page

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -11,8 +11,13 @@ final class WelcomeController extends AbstractController
     #[Route('/', name: 'app_welcome')]
     public function index(): Response
     {
+        $currentDate = new \DateTimeImmutable(
+            'now',
+            new \DateTimeZone('Europe/Paris')
+        );
+
         return $this->render('welcome/index.html.twig', [
-            'controller_name' => 'WelcomeController',
+            'currentDate' => $currentDate->format('d/m/Y'),
         ]);
     }
 }
