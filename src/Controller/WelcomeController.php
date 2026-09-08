9da24bd2aa232c3fdfdf78792301c6fc988968db
bouquet <bouquet@sio.ndlp>
2026-09-08 11:21:18 +0200

    [NEW] Add translation

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -5,19 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class WelcomeController extends AbstractController
 {
     #[Route('/', name: 'app_welcome')]
-    public function index(): Response
+    public function index(TranslatorInterface $translator): Response
     {
         $currentDate = new \DateTimeImmutable(
             'now',
             new \DateTimeZone('Europe/Paris')
         );
 
+        $appName = $translator->trans('DigitalFirstSteps');
+
         return $this->render('welcome/index.html.twig', [
             'currentDate' => $currentDate->format('d/m/Y'),
+            'appName' => $appName,
         ]);
     }
 }
